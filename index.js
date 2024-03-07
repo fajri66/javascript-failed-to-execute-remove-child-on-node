c4274ce801cf2807bbb884563786baf42cf4ee6c
bobbyhadz <opmftw@gmail.com>
2024-03-07 11:01:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+// ✅ Passing a direct child of the element to `removeChild()`
+
+const parent = document.getElementById('parent');
+
+const child = document.getElementById('child');
+
+parent.removeChild(child);
